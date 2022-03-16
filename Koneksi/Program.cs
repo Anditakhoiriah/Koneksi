a542de14e90e3d6d233d2a256a2227474c711c8e
AnditaKhoiriah <andita.khoiriah.ft20@mail.umy.ac.id>
2022-03-16 14:14:39 +0700

    Add project files.

---
+++ Koneksi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Koneksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Connecting();
+        }
+        public void Connecting()
+        {
+            using (
+                SqlConnection con = new SqlConnection("data source=ANDITAKHRH\\ANDITAKHRH;database=ProdiTI;Integrated Security=true")
+            )
+            {
+                con.Open();
+                Console.WriteLine("Berhasil Terhubung Ke database!");
+            }
+        }
+    }
+}
